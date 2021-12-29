30b8d2a0e79edbac15b2d3164d0d075535eedc6f
Leonid <leomrg861@gmail.com>
2021-12-29 14:07:32 +0200

    edit student

--- src/EditStudent.js
+++ src/EditStudent.js
@@ -1,12 +1,12 @@
 import React, { Component, useState, useEffect } from "react";
-import  { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { service as studentService } from "./StudentsService";
 
 export default function EditStudent(props) {
   const [inputs, setInputs] = useState(props.student || {});
   let params = useParams();
   let id = params.id;
-  
+
   useEffect(async () => {
     if (!props.student && id) {
       const result = await studentService.getStudent(id);
@@ -23,65 +23,98 @@ export default function EditStudent(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(id){
+    alert(JSON.stringify(inputs));
+    if (id) {
       //put
-    }
-    else{
+    } else {
       //post
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input
-          type="text"
-          name="firstName"
-          value={inputs.firstName || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          type="text"
-          name="lastName"
-          value={inputs.lastName || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Gender:
-        <select
-          name="gender"
-          value={inputs.gender || "Male"}
-          onChange={handleChange}
-        >
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-      </label>
-      <label>
-        Age:
-        <input
-          type="number"
-          name="age"
-          value={inputs.age || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Department:
-        <input
-          type="text"
-          name="department"
-          value={inputs.department || ""}
-          onChange={handleChange}
-        />
-      </label>
+    <>
+      <h1 className="my-5 me-5 text-center">{id ? "Edit" : "Add"} Student</h1>
+      <div className="row">
+        <div className="col-md-2"></div>
+        <form onSubmit={handleSubmit} className="col-md-6">
+          <div class="mb-3">
+            <label for="firstname" class="form-label">
+              First Name:
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              className="form-control"
+              id="firstname"
+              value={inputs.firstName || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="lastname" class="form-label">
+              Last Name:
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              className="form-control"
+              id="lastname"
+              value={inputs.lastName || ""}
+              onChange={handleChange}
+            />
+          </div>
 
-      <input type="submit" />
-    </form>
+          <div class="mb-3">
+            <label for="gendeer" class="form-label">
+              Gender:
+            </label>
+            <select
+              name="gender"
+              id="gender"
+              className="form-select"
+              value={inputs.gender || "Male"}
+              onChange={handleChange}
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="age" class="form-label">
+              Age:
+            </label>
+            <input
+              type="number"
+              name="age"
+              id="age"
+              className="form-control"
+              value={inputs.age || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="department" class="form-label">
+              Department:
+            </label>
+            <input
+              type="text"
+              name="department"
+              id="department"
+              className="form-control"
+              value={inputs.department || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary mt-4"
+            style={{ width: "100px" }}
+          >
+            {id ? "Edit" : "Add"}
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
